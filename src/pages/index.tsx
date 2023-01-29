bd7dac0fb158e277a8a09cf465effc8f338bbb00
LiamFlamel <117555054+LiamFlamel@users.noreply.github.com>
2023-01-29 12:34:11 +0900

    Setup all settings

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        {/* Information */}
+        <title>Ingnum Networks</title>
+
+        {/* Settings */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+    </>
+  )
+}
