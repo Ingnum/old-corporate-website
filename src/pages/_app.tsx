7758fc0cdbfa13982642ecf6c30dd529573f813e
LiamFlamel <117555054+LiamFlamel@users.noreply.github.com>
2023-01-31 20:52:27 +0900

    Add newly dependency boxicons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import type { AppProps } from 'next/app'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'boxicons/css/boxicons.min.css'
+
 import '../styles/global.css'
 
 export default function App({ Component, pageProps }: AppProps) {
