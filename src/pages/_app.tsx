90c9839358eeb529377d26538b15e765f8ec0be6
LiamFlamel <117555054+LiamFlamel@users.noreply.github.com>
2023-02-01 21:33:13 +0900

    Newly add dependency for increasing icons > remixicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'boxicons/css/boxicons.min.css'
+import 'remixicon/fonts/remixicon.css'
 
 import '../styles/global.css'
 
