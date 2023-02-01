3d9e0c6a65444a59eca42a036ea71ee1971117c7
LiamFlamel <117555054+LiamFlamel@users.noreply.github.com>
2023-02-01 21:23:36 +0900

    Add top section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,9 +15,33 @@ export default function Home() {
       </Head>
 
       <Header />
-      <main>
-        <br /><br /><br />
-      </main>
+
+      {/* Section: Top */}
+      <section id="top">
+        <div id="topCarousel" data-bs-interval="5000" className="carousel slide carousel-fade" data-bs-ride="carousel">
+          <ol className="carousel-indicators" id="top-carousel-indicators" />
+          <div className="carousel-inner" role="listbox">
+            <div className="carousel-item active" style={{ 'backgroundImage': 'url(/images/slide/slide-1.jpg)'}} >
+              <div className="carousel-container">
+                <div className="container">
+                  <h2 className="animate__animated animate__fadeInDown"><span>イングナム</span>へようこそ！</h2>
+                  <p className="animate__animated animate__fadeInUp">サーバーホスティング事業を中心とし、様々な事業を展開しています。</p>
+                  <a href="#about" className="btn-get-started animate__animated animate__fadeInUp scrollto">詳細を見る</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <a className="carousel-control-prev" href="#topCarousel" role="button" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true" />
+          </a>
+
+          <a className="carousel-control-next" href="#topCarousel" role="button" data-bs-slide="next">
+            <span className="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true" />
+          </a>
+        </div>
+      </section>
+
       <Footer />
     </>
   )
